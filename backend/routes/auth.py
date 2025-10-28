44deb8f116072a3edf37e56aeea88cfdb245e403
sohamsontakkeai-creator <sohamsontakke.ai@gmail.com>
2025-10-28 15:38:42 +0530

    Update auth.py

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -7,6 +7,7 @@ from flask_mail import Message
 from models.user import User, UserStatus, db
 from models.password_reset_token import PasswordResetToken
 from werkzeug.security import check_password_hash, generate_password_hash
+import threading
 
 auth_bp = Blueprint('auth', __name__)
 
